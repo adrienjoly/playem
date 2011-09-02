c3ea8ac5a866fae8fa1c396f32280155c695f183
Adrien Joly <adrien@whyd.com>
2011-09-03 00:58:06 +0300

    Edited playem.js via GitHub

--- playem.js
+++ playem.js
@@ -66,7 +66,8 @@ $(window).ready(function() {
 			+ '<span class="postShareFB" onclick="shareVideo()">&nbsp;</span>')
 		//swfobject.embedSWF(vid.url, 'videoEmbed', '425', '344', '9.0.0', '', flashvars, params, attributes);
 		var url = vid.url.replace("/v/", "/embed/") + '?autoplay=1';
-		$('videoEmbed').html('<iframe id="videoEmbed" src="'+url+'" width="425" height="344" frameborder="0" class="youtube-player" type="text/html" ></iframe>');
+		
+		$('#videoEmbed').html('<iframe id="videoEmbed" src="'+url+'" width="425" height="344" frameborder="0" class="youtube-player" type="text/html" ></iframe>');
     
 		window.playNext = function() {
 			//console.log("playNext");
