8f9787fe2ff6b8e843f5331a330ad58c91895a2f
Adrien Joly <adrien@whyd.com>
2012-08-04 12:13:37 +0200

    google analytics in bookmarklet

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -293,4 +293,15 @@
 	}
 	
 	include(urlPrefix + "/bookmarklet.css" + urlSuffix, initplayemBookmarklet);
+
+	// load google analytics
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-1858235-3']);
+	_gaq.push(['_trackPageview', '/js/bookmarklet.js']);
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
 })();
