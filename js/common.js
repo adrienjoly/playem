ed7a24bba0547bc5bff1d8a9aad98a22ac7697e7
Adrien Joly <adrien.joly@gmail.com>
2014-05-10 13:04:51 +0200

    parsing #design=englar as url parameters => several additions

--- js/common.js
+++ js/common.js
@@ -33,9 +33,18 @@ _gaq.push(['_trackPageview']);
 window.olark||(function(i){var e=window,h=document,a=e.location.protocol=="https:"?"https:":"http:",g=i.name,b="load";(function(){e[g]=function(){(c.s=c.s||[]).push(arguments)};var c=e[g]._={},f=i.methods.length; while(f--){(function(j){e[g][j]=function(){e[g]("call",j,arguments)}})(i.methods[f])} c.l=i.loader;c.i=arguments.callee;c.f=setTimeout(function(){if(c.f){(new Image).src=a+"//"+c.l.replace(".js",".png")+"&"+escape(e.location.href)}c.f=null},20000);c.p={0:+new Date};c.P=function(j){c.p[j]=new Date-c.p[0]};function d(){c.P(b);e[g](b)}e.addEventListener?e.addEventListener(b,d,false):e.attachEvent("on"+b,d); (function(){function l(j){j="head";return["<",j,"></",j,"><",z,' onl'+'oad="var d=',B,";d.getElementsByTagName('head')[0].",y,"(d.",A,"('script')).",u,"='",a,"//",c.l,"'",'"',"></",z,">"].join("")}var z="body",s=h[z];if(!s){return setTimeout(arguments.callee,100)}c.P(1);var y="appendChild",A="createElement",u="src",r=h[A]("div"),G=r[y](h[A](g)),D=h[A]("iframe"),B="document",C="domain",q;r.style.display="none";s.insertBefore(r,s.firstChild).id=g;D.frameBorder="0";D.id=g+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){D.src="javascript:false"} D.allowTransparency="true";G[y](D);try{D.contentWindow[B].open()}catch(F){i[C]=h[C];q="javascript:var d="+B+".open();d.domain='"+h.domain+"';";D[u]=q+"void(0);"}try{var H=D.contentWindow[B];H.write(l());H.close()}catch(E){D[u]=q+'d.write("'+l().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}c.P(2)})()})()})({loader:(function(a){return "static.olark.com/jsclient/loader0.js?ts="+(a?a[1]:(+new Date))})(document.cookie.match(/olarkld=([0-9]+)/)),name:"olark",methods:["configure","extend","declare","identify"]});
 olark.identify('6373-737-10-6529');
 
+// utility classes and functions
+
 function loadCss(path, cb){
 	$.get(path, function(response){
 		$('<style></style>').text(response).appendTo('head');
+		cb();
+	});
+}
+
+function loadJS(path, cb){
+	$.get(path, function(response){
+		$('<script></script>').text(response).appendTo('body');
 		cb();
 	});
 }
@@ -52,10 +61,36 @@ function WhenDone(cb){
 	}
 };
 
+function keepLettersOnly(str) {
+	return (""+str).replace(/[^\w]/g, "");
+}
+
+// init
+
 function initPlayemUI(uiDir){
+	var uiDir = uiDir || "/ui-default";
 	var makeCallback = new WhenDone(function(){
 		$("body")[0].className = "welcome";
 	});
 	initFB(makeCallback());
 	loadCss(uiDir+"/styles.css", makeCallback());
+	loadJS(uiDir+"/ui.js", makeCallback());
 }
+
+var DEFAULTS = {
+	design: "default"
+};
+
+(function init(p){
+	for (var i in DEFAULTS)
+		if (!p.hasOwnProperty(i))
+			p[i] = DEFAULTS[i];
+	initPlayemUI("/ui-" + keepLettersOnly(p.design));
+})((function readHashParams(){
+	var params = {}, strings = window.location.href.split(/[#&]+/).slice(1);
+	for (var i in strings) {
+		var splitted = strings[i].split("=");
+		params[decodeURIComponent(splitted[0])] = typeof(splitted[1]) == "string" ? decodeURIComponent(splitted[1]) : null;
+	}
+	return params;
+})());
---
+++ ui-englar/ui.js
@@ -0,0 +1,4 @@
+$("#logo").prependTo("#welcome");
+//$("#socialButtons").appendTo("#welcome");
+$("#txtCallToAction").insertAfter($("#fbconnect").parent());
+$("#footer").html('© Copyright 2011-2014 - Playem Concept & Code by <a href="http://adrienjoly.com/" target="_blank">Adrien Joly</a> | Design by <a href="http://www.englar-design.com/" target="_blank">EnglarDesign</a>');
